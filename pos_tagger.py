9ed712247a4efb2e9b669325a351e596b22f3d52
Joshua Stephenson <josh@brightmediums.com>
2024-11-20 14:13:45 -0500

    simplified pos_tagger to read only from STDIN

--- pos_tagger.py
+++ pos_tagger.py
@@ -2,20 +2,9 @@
 import sys
 import spacy
 from collections import Counter, defaultdict
-import argparse
-
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument('-f', '--file', type=str)
-group.add_argument('-i', '--stdin', action='store_true')
-args = parser.parse_args()
 
 es_nlp = spacy.load('es_core_news_sm')
-if args.file is not None:
-    with open(args.file) as f:
-        text = f.read()
-elif args.stdin:
-    text = sys.stdin.read()
+text = sys.stdin.read()
 
 spanish = es_nlp(text.strip())
 info = defaultdict(list)
