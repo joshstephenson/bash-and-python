9eaee92d9b966eb775397edcf0784fd9369248f3
Joshua Stephenson <josh@brightmediums.com>
2024-11-20 13:53:40 -0500

    simplification

--- pos_tagger.py
+++ pos_tagger.py
@@ -2,31 +2,30 @@
 import sys
 import spacy
 from collections import Counter, defaultdict
+import argparse
 
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-f', '--file', type=str)
+group.add_argument('-i', '--stdin', action='store_true')
+args = parser.parse_args()
+print(args)
 
-def main(opts):
-    es_nlp = spacy.load('es_core_news_sm')
-    if opts.file is not None:
-        with open(opts.file) as f:
-            text = f.read()
-    else:
-        text = sys.stdin.read()
+es_nlp = spacy.load('es_core_news_sm')
+if args.file is not None:
+    with open(args.file) as f:
+        text = f.read()
+elif args.stdin:
+    text = sys.stdin.read()
 
-    spanish = es_nlp(text.strip())
-    info = defaultdict(list)
-    counter = Counter([token.pos_ for token in spanish])
-    for token in spanish:
-        if token.text:
-            info[token.pos_].append(token.text)
+spanish = es_nlp(text.strip())
+info = defaultdict(list)
+counter = Counter([token.pos_ for token in spanish])
+for token in spanish:
+    if token.text:
+        info[token.pos_].append(token.text)
 
-    for pos, words in info.items():
-        count = counter[pos]
-        sys.stdout.write(f'{pos} ({count}): {sorted(words)}' + '\n')
+for pos, words in info.items():
+    count = counter[pos]
+    sys.stdout.write(f'{pos} ({count}): {sorted(words)}' + '\n')
 
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', type=str, required=False)
-    args = parser.parse_args()
-    main(args)
--- stt.py
+++ stt.py
@@ -2,22 +2,19 @@
 
 import speech_recognition as sr
 import sys
+import argparse
 
-def get_text(filename, language_code):
-    r = sr.Recognizer()
-    with sr.AudioFile(filename) as source:
-        d = r.record(source)
-        text = r.recognize_google(d, language=language_code)
-        sys.stdout.write(text + '\n')
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", '--file', required=True, type=str)
+parser.add_argument('-l', '--language', required=True, type=str, default='es-LA',
+                    help='Choose a language code from: https://www.science.co.il/language/Locale-codes.php')
+args = parser.parse_args()
+if not args.file.endswith('wav'):
+    parser.error('file must be wave file')
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", '--input', required=True, type=str)
-    parser.add_argument('-l', '--language', required=True, type=str, default='es-LA',
-                        help='Choose a language code from: https://www.science.co.il/language/Locale-codes.php')
-    args = parser.parse_args()
-    filename = args.input
-    if not filename.lower().endswith('wav'):
-        raise BaseException('This file needs a wav audio file. Please use convert.py first.')
-    get_text(filename,args.language)
+r = sr.Recognizer()
+with sr.AudioFile(args.file) as source:
+    d = r.record(source)
+    text = r.recognize_google(d, language=args.language)
+    sys.stdout.write(text + '\n')
+
